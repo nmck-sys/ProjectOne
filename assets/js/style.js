a4e6db68ea21915d59a414c4e1566127d04ed918
HA_GO\antic <nmckinzie@smcm.edu>
2024-08-05 22:28:49 -0400

    added weather API

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,8 +1,24 @@
 // Define the API URL
 const apiUrl = 'https://dogapi.dog/api/v2/facts';
 
+const weatherMan = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m';
+
 // Make a GET request
 fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+  fetch(weatherMan)
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
