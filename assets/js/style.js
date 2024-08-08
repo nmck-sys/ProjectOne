4afadab9028a5896b8e42f4ce056c0a6f91385ba
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-08-07 19:32:44 -0500

    fixing js file

--- assets/js/style.js
+++ assets/js/style.js
@@ -17,24 +17,3 @@ fetch(apiUrl)
   .catch(error => {
     console.error('Error:', error);
   });
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-
-	//Weather Function Uses Tokyo Japan as a placeholder
-
-    		async function getWeather() {
-		
-			const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m');
-		
-			const data = await response.json();
-			
-			const weatherElement = document.getElementById('weather');
-			const temperature = data.hourly.temperature_2m[0];
-
-			weatherElement.innerHTML = `<h4>Temperature: ${temperature}°C</h4>`; 
-
-	}
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> a1b25725d7360c588936d9b4cfc9c3f2005961ef
